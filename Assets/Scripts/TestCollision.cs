393145cca904c73d61a67349fb60ed764575000a
dannykoh <danny.koh@unity3d.com>
2023-10-22 18:07:53 +0900

    Added Raycast

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -4,14 +4,52 @@ using UnityEngine;
 
 public class TestCollision : MonoBehaviour
 {
-     void Start()
+    void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        // Local <--> World <--> Viewport <--> Screen
+
+        //RayCastForward();
+
+        //Debug.Log(Input.mousePosition); // Screen coordinate
+
+        //Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); // Viewport coordinate
+
+        #region Ray way
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100, Color.red, 1f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log($"Raycast Camera @{hit.collider.name}");
+            }
+        }
+        #endregion
+
+        #region WorldPoint way
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        //    Vector3 mouseDir = mousePos - Camera.main.transform.position;
+        //    mouseDir = mouseDir.normalized;
+
+        //    Debug.DrawRay(Camera.main.transform.position, mouseDir * 100, Color.red, 1.0f);
+
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(Camera.main.transform.position, mouseDir, out hit, 100))
+        //    {
+        //        Debug.Log($"Raycast Camera @{hit.collider.name}");
+        //    }
+        //}
+        #endregion
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -22,5 +60,16 @@ public class TestCollision : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"Trigger! @{other.name}");
+    }
+
+    private void RayCastForward()
+    {
+        Vector3 lookForward = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position + Vector3.up, lookForward * 10, Color.red);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + Vector3.up, lookForward, out hit, 10))
+        {
+            Debug.Log($"RayCast! @{hit.collider.name}");
+        }
     }
 }
