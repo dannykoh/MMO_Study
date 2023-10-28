e917b49ee26076b0d4d26b707deee2bfb5b7f590
Danny Koh <mariah04@naver.com>
2023-10-28 22:58:22 +0900

    Up to Animation Event

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -24,8 +24,6 @@ public class PlayerController : MonoBehaviour
         Managers.Input.MouseAction -= OnMouseClicked;
     }
 
-    float wait_run_ratio = 0;
-
     public enum PlayerState
     {
         Dead,
@@ -53,9 +51,7 @@ public class PlayerController : MonoBehaviour
 
     private void UpdateIdle()
     {
-        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);
-        anim.SetFloat("wait_run_ratio", wait_run_ratio);
-        anim.Play("WAIT_RUN");
+        anim.SetFloat("speed", 0);
     }
 
     private void UpdateMoving()
@@ -72,9 +68,7 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
         }
 
-        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
-        anim.SetFloat("wait_run_ratio", wait_run_ratio);
-        anim.Play("WAIT_RUN");
+        anim.SetFloat("speed", _speed);
     }
 
     private void UpdateDead()
